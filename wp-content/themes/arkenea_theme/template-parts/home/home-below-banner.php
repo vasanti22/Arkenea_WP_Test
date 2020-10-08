c1942031d9cda13aeb461d2a95a07ad24c6a2caf
vasanti22 <vasanti.vvp@gmail.com>
2020-10-08 23:34:16 +0530

    new changes

--- wp-content/themes/arkenea_theme/template-parts/home/home-below-banner.php
+++ wp-content/themes/arkenea_theme/template-parts/home/home-below-banner.php
@@ -1,6 +1,6 @@
 <div class="content-block1 common-margin">
-    <h2> the challange</h2>
-    <p>The phician lead company witnessed a high rate of hospital admissions, morbidity and early mortality rates for the lack a collaborative platform between patients, care givers and family members. </p>
+    <h2> the challenge</h2>
+    <p>The physician lead company witnessed a high rate of hospital admissions, morbidity and early mortality rates for the lack a collaborative platform between patients, care givers and family members. </p>
 </div>
 
 <div class="content-block1-slides">
@@ -21,10 +21,42 @@
 </div>
 
 <div class="solution-content common-margin">
-    <img src="<?php bloginfo('template_url'); ?>/assets/img/two.jpg">
-    <img src="<?php bloginfo('template_url'); ?>/assets/img/two.jpg">
-    <img src="<?php bloginfo('template_url'); ?>/assets/img/two.jpg">
-    <div class="blue-bg"></div>
+    <div class="solution-content-wrap">
+     <img src="<?php bloginfo('template_url'); ?>/assets/img/two.jpg">
+     <img src="<?php bloginfo('template_url'); ?>/assets/img/two.jpg">
+     <img src="<?php bloginfo('template_url'); ?>/assets/img/two.jpg">
+ </div>
+ <div class="blue-bg"></div>
+ <div class="blue-bg1"></div>
 </div>
+
+<div class="common-padding grey-bg">
+    <div class="solution-highlights">
+        <h2>Key Highlights of the Solution</h2>
+        <ul>
+            <li>Timely reminders of appointments, daily medication and tasks</li>
+            <li>Easy 2 way communication between patiten and providers</li>
+            <li>Timely reminders of appointments, daily medication and tasks</li>
+            <li>Timely reminders of appointments, daily medication and tasks</li>
+            <li>Timely reminders of appointments, daily medication and tasks</li>
+        </ul>
+    </div>
+</div>
+
+<div class="content-block1 common-margin">
+    <h2>word from our client</h2>
+    <div class="client-review">
+        <div class="client-logo"><img src="<?php bloginfo('template_url'); ?>/assets/img/sym-logo.svg"></div>
+        <p class="review-txt">"Arkenea has done what they can to make each iteration more seamless than the previous one" </p>
+        <div class="client-info">
+            <span class="client-img"></span>
+            <span class="name">dr. adrian pieleanu,</span>
+            <span class="designation"><em>founder, TeleMedHome</em></span>
+
+        </div>
+    </div>
+    
+</div>
+
 
 
